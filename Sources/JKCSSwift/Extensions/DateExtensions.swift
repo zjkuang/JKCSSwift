d22590cab5fa9fdbf3413e2f41efb311f11774df
John Kuang <john.kuang@robotsandpencils.com>
2020-06-16 18:08:29 -0600

    Date extension

---
+++ Sources/JKCSSwift/Extensions/DateExtensions.swift
@@ -0,0 +1,32 @@
+//
+//  DateExtensions.swift
+//  Practice001
+//
+//  Created by Zhengqian Kuang on 2020-06-16.
+//  Copyright © 2020 Kuang. All rights reserved.
+//
+
+import Foundation
+
+public extension Date {
+    func yyyyMMddHHmmss() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+        let str = formatter.string(from: self)
+        return str
+    }
+    
+    func yyyyMMddHHmmssSSS() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd HH:mm:ss.SSS"
+        let str = formatter.string(from: self)
+        return str
+    }
+    
+    func yyyyMMdd() -> String {
+        let formatter = DateFormatter()
+        formatter.dateFormat = "yyyy-MM-dd"
+        let str = formatter.string(from: self)
+        return str
+    }
+}
